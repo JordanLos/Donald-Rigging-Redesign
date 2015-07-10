268d81128bde900673385ea339280f6da9baeb6c
jordan <jordan.los@hotmail.com>
2015-07-10 13:17:24 -0600

    Service images can now be toggled by clicking the thumbnail as well as the image itself

--- js/animations.js
+++ js/animations.js
@@ -1,45 +1,69 @@
 /*****Image Gallery*******
 *************************/
-/*PURPOSE: Each of the 6 services has 2 images positioned identically 
-with equal z-index. Clicking each image will fade one image out with fading the other in.*/
+/*PURPOSE: Toggle between the 2 viewable images of each service. The code toggles the image by fading out the front image to reveal the back image, and fades in the front image to hide the back image. Users can toggle between the images by clicking the image itself or its thumbnail. 
+*/
 
 document.onreadystatechange = function () {
 	if (document.readyState === 'complete') {
 
-	//arraya of elements used to fade in/out and restyle thumbnails
+	//array front images for each service to be faded in or out
 	var image1 = document.getElementsByClassName('service-image-1');
+	//array of 1st thumbnail image for each service
 	var thumb1 = document.getElementsByClassName('service-thumb-1');
+	//array of 2nd thumbnail image for each service
 	var thumb2 = document.getElementsByClassName('service-thumb-2');
 
-	//Fades front image out and back image in for the selected service when clicking 'image1'
-	function fadeToBack(serviceIndex) {
+	//Fades out front image, revealing back image
+	function fadeOut(serviceIndex) {
 		TweenLite.to(image1[ serviceIndex ], 0.2, {opacity: 0});
 		TweenLite.to(thumb1[ serviceIndex ], 0, {borderColor: "transparent"});
 		TweenLite.to(thumb2[ serviceIndex ], 0, {borderColor: "white"});
 	}
-	//Fades back image out and front image in for the selected service when clicking 'image1'
-	function fadeToFront(serviceIndex) {	
+	//Fades in front image, hiding back image
+	function fadeIn(serviceIndex) {	
 		TweenLite.to(image1[ serviceIndex ], 0.2, {opacity: 1});
 		TweenLite.to(thumb2[ serviceIndex ], 0, {borderColor: "transparent"});
 		TweenLite.to(thumb1[ serviceIndex ], 0, {borderColor: "white"});
 	}
-	//Alternates between the two 'fadeTo' functions. arrayIndex is the same for both the clickCount array and the service index
-	function toggleFade(arrayIndex) {
-		image1[ arrayIndex ].onclick = function() {
-			if (clickCount[arrayIndex] == 0) {
-				clickCount[arrayIndex] = 1;
-				return fadeToBack(arrayIndex);
-			} else if (clickCount[arrayIndex] == 1) {
-				clickCount[arrayIndex] = 0;
-				return fadeToFront(arrayIndex);
+	//Creates an array for toggleByImage to test whether the 1st image is faded in or out
+	var clickState = []
+	//creates a clickState index for each service
+	for (i = 0; i < image1.length; i++) {
+		clickState.push(0);
+	}
+
+	//Calls FadeOut by clicking 1st thumbnail and FadeIn by clicking the 2nd thumbnail. Also updates the clickState for toggleByImage
+	function toggleByThumb(i) {
+		thumb1[ i ].onclick = function() {
+		clickState[ i ] = 0;
+		return fadeIn(i);
+		};
+		thumb2[ i ].onclick = function() {
+		clickState[ i ] = 1;
+			return fadeOut(i);
+		};
+	}
+	//an array to store the thumbnail pairs for each service
+	var allThumbs = []
+	//adds a ToggleByThumb onclick event for each service
+	for (i = 0; i < thumb1.length; i++) {
+		allThumbs.push(toggleByThumb(i));
+	}
+	//Fades the front service image in or out when the user clicks on the image
+	function toggleByImage( i ) {
+		image1[ i ].onclick = function() {
+			if (clickState[ i ] == 0) {
+				clickState[ i ] = 1;
+				return fadeOut( i );
+			} else if (clickState[ i ] == 1) {
+				clickState[ i ] = 0;
+				return fadeIn( i );
 			}
 		};
 	}
-	//An array giving each service it's own index for counting clicks
-	var clickCount = [0, 0, 0 ,0 , 0, 0]
-	//Creates a toggleFade function for each service
-	for (i = 0; i < clickCount.length; i++) {
-		toggleFade(i);
+	//Creates a toggleByImage function for each service
+	for (i = 0; i < image1.length; i++) {
+		toggleByImage(i);
 	}
 
 	/****LANDING ANIMATION FIX *****/
@@ -50,3 +74,4 @@ document.onreadystatechange = function () {
 
 	}
 };
+
